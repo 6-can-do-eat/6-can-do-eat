2ab7cc42259adc1fd43d1e740e096d280fd2a85c
seongwop <psw921@gmail.com>
2026-04-17 15:03:58 +0900

    chore: initialize project

---
+++ src/main/java/com/team6/backend/SixCanDoEatApplication.java
@@ -0,0 +1,13 @@
+package com.team6.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixCanDoEatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SixCanDoEatApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/team6/backend/global/infrastructure/entity/BaseEntity.java
@@ -0,0 +1,56 @@
+package com.team6.backend.global.infrastructure.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(nullable = false, updatable = false)
+    private String createdBy;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    @Column(nullable = false)
+    private String updatedBy;
+
+    @Column
+    private LocalDateTime deletedAt;
+
+    @Column
+    private String deletedBy;
+
+    public void markDeleted(String deletedBy) {
+        this.deletedAt = LocalDateTime.now();
+        this.deletedBy = deletedBy;
+    }
+
+    public void restore() {
+        this.deletedAt = null;
+        this.deletedBy = null;
+    }
+
+    public boolean isDeleted() {
+        return this.deletedAt != null;
+    }
+}
+
