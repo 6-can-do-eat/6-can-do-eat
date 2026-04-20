3abc23c723858f4ead4486303e74dd463e1365a3
qkrwns1478 <qkrwns1478@gmail.com>
2026-04-20 16:09:57 +0900

    feat: 엔티티 생성 - 가게: Store - 상품: Menu - 지역: Area - 카테고리: Category

---
+++ src/main/java/com/team6/backend/area/domain/entity/Area.java
@@ -0,0 +1,45 @@
+package com.team6.backend.area.domain.entity;
+
+import com.team6.backend.store.domain.entity.Store;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "p_area")
+public class Area {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "area_id")
+    private UUID id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 50)
+    private String city;
+
+    @Column(nullable = false, length = 50)
+    private String district;
+
+    @Column(nullable = false)
+    private boolean is_active;
+
+    @OneToMany
+    private List<Store> stores;
+
+    public Area(String name, String city, String district, boolean is_active) {
+        this.name = name;
+        this.city = city;
+        this.district = district;
+        this.is_active = is_active;
+    }
+
+}
