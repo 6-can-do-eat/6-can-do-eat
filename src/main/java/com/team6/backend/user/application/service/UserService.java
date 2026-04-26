4bb9366a8a2410cc4b47626255e13af4a4151573
jimin Kim <gjimin26@gmail.com>
2026-04-26 14:36:55 +0900

    기본 설정

---
+++ src/main/java/com/team6/backend/user/application/service/UserService.java
@@ -0,0 +1,4 @@
+package com.team6.backend.user.application.service;
+
+public class UserService {
+}
---
+++ src/main/java/com/team6/backend/user/presentation/cotrolller/UserController.java
@@ -0,0 +1,4 @@
+package com.team6.backend.user.presentation.cotrolller;
+
+public class UserController {
+}
