e5669e3d67d68fac981fa820429c7be24edbad2e
seongwop <psw921@gmail.com>
2026-04-27 10:33:14 +0900

    refactor: Order/Payment 테스트 코드 추가

--- src/main/java/com/team6/backend/payment/domain/Payment.java
+++ src/main/java/com/team6/backend/payment/domain/Payment.java
@@ -45,7 +45,7 @@ public class Payment extends BaseEntity {
     }
 
     public void updatePaymentStatus(PaymentStatus paymentStatus) {
-        if (this.status.canChangeTo(paymentStatus)) {
+        if (!this.status.canChangeTo(paymentStatus)) {
             throw new ApplicationException(PaymentErrorCode.PAYMENT_INVALID_STATUS);
         }
         this.status = paymentStatus;
