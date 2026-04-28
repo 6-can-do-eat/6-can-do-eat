4e007de5c0d03db5480b21995010eab4eb35e70b
seongwop <psw921@gmail.com>
2026-04-28 14:23:59 +0900

    feat: 결제 생성 동시성/멱등성 문제 upsert 처리

--- src/main/java/com/team6/backend/payment/domain/PaymentRepository.java
+++ src/main/java/com/team6/backend/payment/domain/PaymentRepository.java
@@ -3,8 +3,9 @@ package com.team6.backend.payment.domain;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -15,4 +16,40 @@ public interface PaymentRepository extends JpaRepository<Payment, UUID> {
 
     Page<Payment> findAll(Pageable pageable);
     Page<Payment> findAllByOrder_User_Id(UUID userId, Pageable pageable);
+
+    @Modifying
+    @Query(value = """
+        insert into p_payment (
+            id,
+            order_id,
+            payment_key,
+            payment_type,
+            status,
+            amount,
+            created_at,
+            created_by,
+            updated_at,
+            updated_by
+        )
+        values (
+            :paymentId,
+            :orderId,
+            :paymentKey,
+            'CARD',
+            'COMPLETED',
+            :amount,
+            current_timestamp,
+            :auditor,
+            current_timestamp,
+            :auditor
+        )
+        on conflict do nothing
+        """, nativeQuery = true)
+    int insertPaymentIfAbsent(
+            UUID paymentId,
+            UUID orderId,
+            String paymentKey,
+            Long amount,
+            String auditor
+    );
 }
